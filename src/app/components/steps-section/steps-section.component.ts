f67e101e558876fc44777cd03fcefeb9d370554b
MathiTarazonad <vladitarazona@gmail.com>
2023-06-21 14:40:38 -0400

    Create steps section component with mobile styles

---
+++ src/app/components/steps-section/steps-section.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { StepComponent } from '../step/step.component';
+import { CommonModule } from '@angular/common'
+
+interface Step {
+  stepDesc: string;
+  stepNumber: number;
+}
+
+@Component({
+  standalone: true,
+  imports: [StepComponent, CommonModule],
+  selector: 'steps-section',
+  templateUrl: './steps-section.component.html',
+  styleUrls: ['./steps-section.component.css'],
+})
+
+export class StepsSectionComponent implements OnInit {
+
+  steps: Step[] = [
+    {stepNumber: 1, stepDesc: 'Your Info'},
+    {stepNumber: 2, stepDesc: 'Select Plan'},
+    {stepNumber: 3, stepDesc: 'Add-ons'},
+    {stepNumber: 4, stepDesc: 'Summary'}
+  ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
