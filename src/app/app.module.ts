3d0450a87341ac9e49f222f9591ab0a84ca98ba4
MathiTarazonad <vladitarazona@gmail.com>
2023-06-21 14:42:01 -0400

    Fix app template and add some initial styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StepsSectionComponent } from './components/steps-section/steps-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StepsSectionComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
