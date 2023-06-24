8fcc03830133ad1344c8576d9148e0dd5062a2a1
MathiTarazonad <vladitarazona@gmail.com>
2023-06-23 20:54:22 -0400

    Add first step form component to template

--- src/app/components/form-section/form-section.component.ts
+++ src/app/components/form-section/form-section.component.ts
@@ -1,13 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FirstFormStepComponent } from '../first-form-step/first-form-step.component';
 
 @Component({
   selector: 'app-form-section',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FirstFormStepComponent],
   templateUrl: './form-section.component.html',
   styleUrls: ['./form-section.component.css']
 })
+
 export class FormSectionComponent {
   @Input() currentStep!: number;
   @Input() handleNextStep!: () => void;
