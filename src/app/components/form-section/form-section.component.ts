5288e713d6492918e047d1435896552632e2a7f6
MathiTarazonad <vladitarazona@gmail.com>
2023-06-26 15:44:40 -0400

    Add dateType property and its handler

--- src/app/components/form-section/form-section.component.ts
+++ src/app/components/form-section/form-section.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FirstFormStepComponent } from '../first-form-step/first-form-step.component';
 
+type DateType = 'monthly' | 'yearly'
+
 @Component({
   selector: 'app-form-section',
   standalone: true,
@@ -14,4 +16,9 @@ export class FormSectionComponent {
   @Input() currentStep!: number;
   @Input() handleNextStep!: () => void;
   @Input() handlePreviousStep!: () => void;
+  dateType: DateType = 'monthly'
+
+  handleDateTypeChange = () => {
+    this.dateType = this.dateType === 'monthly' ? 'yearly' : 'monthly';
+  }
 }
