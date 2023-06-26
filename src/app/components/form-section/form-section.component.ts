59e2858ee64c8bb3e202e3adef5072bc9ef0a51a
MathiTarazonad <vladitarazona@gmail.com>
2023-06-26 15:47:12 -0400

    Add date type changer component

--- src/app/components/form-section/form-section.component.ts
+++ src/app/components/form-section/form-section.component.ts
@@ -1,13 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FirstFormStepComponent } from '../first-form-step/first-form-step.component';
+import { DateTypeChangerComponent } from '../date-type-changer/date-type-changer.component';
 
 type DateType = 'monthly' | 'yearly'
 
 @Component({
   selector: 'app-form-section',
   standalone: true,
-  imports: [CommonModule, FirstFormStepComponent],
+  imports: [CommonModule, FirstFormStepComponent, DateTypeChangerComponent],
   templateUrl: './form-section.component.html',
   styleUrls: ['./form-section.component.css']
 })
