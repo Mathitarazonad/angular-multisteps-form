83a6584d3260c6c815934fb0f1b082d47595ac12
MathiTarazonad <vladitarazona@gmail.com>
2023-06-27 09:41:47 -0400

    Create and add second form step component with styles

--- src/app/components/form-section/form-section.component.ts
+++ src/app/components/form-section/form-section.component.ts
@@ -1,14 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FirstFormStepComponent } from '../first-form-step/first-form-step.component';
+import { SecondFormStepComponent } from '../second-form-step/second-form-step.component';
 import { DateTypeChangerComponent } from '../date-type-changer/date-type-changer.component';
 
-type DateType = 'monthly' | 'yearly'
+export type DateType = 'monthly' | 'yearly'
 
 @Component({
   selector: 'app-form-section',
   standalone: true,
-  imports: [CommonModule, FirstFormStepComponent, DateTypeChangerComponent],
+  imports: [CommonModule, FirstFormStepComponent, SecondFormStepComponent, DateTypeChangerComponent],
   templateUrl: './form-section.component.html',
   styleUrls: ['./form-section.component.css']
 })
---
+++ src/app/components/second-form-step/second-form-step.component.ts
@@ -0,0 +1,54 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DateType } from '../form-section/form-section.component';
+
+type SuscriptionType = 'Arcade' | 'Advanced' | 'Pro'
+type MonthlyPriceType = `$${number}/mo`
+type YearlyPriceType = `$${number}/yr`
+type YearlyExtraType = `${number} months free`
+
+interface Suscription {
+  image: string
+  name: SuscriptionType
+  monthlyPrice: MonthlyPriceType
+  yearlyPrice: YearlyPriceType
+  yearlyExtra: YearlyExtraType
+}
+
+
+@Component({
+  selector: 'app-second-form-step',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './second-form-step.component.html',
+  styleUrls: ['./second-form-step.component.css']
+})
+
+export class SecondFormStepComponent {
+  @Input() currentStep!: number;
+  @Input() dateType!: DateType;
+
+  suscriptions: Suscription[] = [
+    {
+      image: '/assets/images/icon-arcade.svg',
+      name: 'Arcade',
+      monthlyPrice: '$9/mo',
+      yearlyPrice: '$90/yr',
+      yearlyExtra: '2 months free'
+    },
+    {
+      image: '/assets/images/icon-advanced.svg',
+      name: 'Advanced',
+      monthlyPrice: '$12/mo',
+      yearlyPrice: '$120/yr',
+      yearlyExtra: '2 months free'
+    },
+    {
+      image: '/assets/images/icon-pro.svg',
+      name: 'Pro',
+      monthlyPrice: '$15/mo',
+      yearlyPrice: '$150/yr',
+      yearlyExtra: '2 months free'
+    }
+  ]
+} 
