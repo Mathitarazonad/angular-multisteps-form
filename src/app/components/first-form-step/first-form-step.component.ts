17cc2f3e43b8b38df3dd0e71b7215342ccf5e57d
MathiTarazonad <vladitarazona@gmail.com>
2023-06-28 15:26:54 -0400

    Add personalInfo property and ngModel with inputs

--- src/app/components/first-form-step/first-form-step.component.ts
+++ src/app/components/first-form-step/first-form-step.component.ts
@@ -1,10 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+export interface PersonalInfo {
+  name: string,
+  email: string,
+  phoneNumber: string,
+}
 
 @Component({
   standalone: true,
+  imports: [FormsModule],
   selector: 'app-first-form-step',
   templateUrl: './first-form-step.component.html',
   styleUrls: ['./first-form-step.component.css']
 })
+
 export class FirstFormStepComponent {
+  personalInfo: PersonalInfo = {
+    name: '',
+    email: '',
+    phoneNumber: ''
+  }
 }
