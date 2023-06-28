365282faa2f2a8709323042357070511e5b9a656
MathiTarazonad <vladitarazona@gmail.com>
2023-06-28 11:38:53 -0400

    Add third-form-step component

--- src/app/components/form-section/form-section.component.ts
+++ src/app/components/form-section/form-section.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FirstFormStepComponent } from '../first-form-step/first-form-step.component';
 import { SecondFormStepComponent } from '../second-form-step/second-form-step.component';
 import { DateTypeChangerComponent } from '../date-type-changer/date-type-changer.component';
+import { ThirdFormStepComponent } from '../third-form-step/third-form-step.component';
 
 export type DateType = 'monthly' | 'yearly'
 
 @Component({
   selector: 'app-form-section',
   standalone: true,
-  imports: [CommonModule, FirstFormStepComponent, SecondFormStepComponent, DateTypeChangerComponent],
+  imports: [CommonModule, FirstFormStepComponent, SecondFormStepComponent, ThirdFormStepComponent, DateTypeChangerComponent],
   templateUrl: './form-section.component.html',
   styleUrls: ['./form-section.component.css']
 })
