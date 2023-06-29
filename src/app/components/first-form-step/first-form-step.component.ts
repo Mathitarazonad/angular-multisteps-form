ba7e01760acdb7533a440ac93333fbba409f8272
MathiTarazonad <vladitarazona@gmail.com>
2023-06-29 09:30:24 -0400

    Add input properties, handler for inputs and function to update it

--- src/app/components/first-form-step/first-form-step.component.ts
+++ src/app/components/first-form-step/first-form-step.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { FormData, InputErrors } from '../form-section/form-section.component';
 
 
 export interface PersonalInfo {
@@ -10,16 +12,34 @@ export interface PersonalInfo {
 
 @Component({
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   selector: 'app-first-form-step',
   templateUrl: './first-form-step.component.html',
   styleUrls: ['./first-form-step.component.css']
 })
 
 export class FirstFormStepComponent {
+  @Input() inputErrors!: InputErrors;
+  @Input() resetInputErrors!: () => void;
+  @Input() updateFormData!: <K extends keyof FormData>(field: K, values: FormData[K]) => void;
   personalInfo: PersonalInfo = {
     name: '',
     email: '',
     phoneNumber: ''
+  }
+
+  handleInput = (e: Event) => {
+    this.resetInputErrors();
+    const target = e.target as HTMLInputElement;
+    const inputField = target.name as keyof PersonalInfo;
+    const inputValue = target.value;
+
+    this.updatePersonalInfo(inputField, inputValue);
+    this.updateFormData('personalInfo', this.personalInfo);
+  }
+
+  updatePersonalInfo = <K extends keyof PersonalInfo>(field: K, value: PersonalInfo[K]) => {
+    const newPersonalInfo = {...this.personalInfo, [field]: value};
+    this.personalInfo = newPersonalInfo;
   }
 }
