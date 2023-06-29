e73f8d8a534c6c2c3711ab75b503cd422e11d785
MathiTarazonad <vladitarazona@gmail.com>
2023-06-29 09:27:04 -0400

    Add property to store form data and function update it

--- src/app/components/form-section/form-section.component.ts
+++ src/app/components/form-section/form-section.component.ts
@@ -1,12 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FirstFormStepComponent } from '../first-form-step/first-form-step.component';
-import { SecondFormStepComponent } from '../second-form-step/second-form-step.component';
+import { FirstFormStepComponent, PersonalInfo } from '../first-form-step/first-form-step.component';
+import { SecondFormStepComponent, Subscription } from '../second-form-step/second-form-step.component';
 import { DateTypeChangerComponent } from '../date-type-changer/date-type-changer.component';
-import { ThirdFormStepComponent } from '../third-form-step/third-form-step.component';
+import { Addon, ThirdFormStepComponent } from '../third-form-step/third-form-step.component';
 import { FourthFormStepComponent } from '../fourth-form-step/fourth-form-step.component';
 
 export type DateType = 'monthly' | 'yearly'
+export interface FormData {
+  personalInfo?: PersonalInfo,
+  subscription?: Subscription
+  addons?: Addon[]
+}
 
 @Component({
   selector: 'app-form-section',
@@ -20,9 +25,19 @@ export class FormSectionComponent {
   @Input() currentStep!: number;
   @Input() handleNextStep!: () => void;
   @Input() handlePreviousStep!: () => void;
-  dateType: DateType = 'monthly'
+  dateType: DateType = 'monthly';
+  formData: FormData = {
+    personalInfo: undefined,
+    subscription: undefined,
+    addons: []
+  };
 
   handleDateTypeChange = () => {
     this.dateType = this.dateType === 'monthly' ? 'yearly' : 'monthly';
+  }
+
+  updateFormData = <K extends keyof FormData>(field: K, values: FormData[K]) => {
+    const updatedFormData = {...this.formData, [field]: values};
+    this.formData = updatedFormData;
   }
 }
