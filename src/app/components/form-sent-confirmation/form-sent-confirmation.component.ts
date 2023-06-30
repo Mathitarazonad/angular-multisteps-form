67942a66b5e80aecd36c79292468358145f9dbdf
MathiTarazonad <vladitarazona@gmail.com>
2023-06-30 09:36:04 -0400

    Create form sent confirmation message component with styles

---
+++ src/app/components/form-sent-confirmation/form-sent-confirmation.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-form-sent-confirmation',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './form-sent-confirmation.component.html',
+  styleUrls: ['./form-sent-confirmation.component.css']
+})
+export class FormSentConfirmationComponent {
+
+}
